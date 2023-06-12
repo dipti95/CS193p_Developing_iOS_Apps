af9a1bc8a4746d839c328d5670913575782a73d4
dipti <dipti.yadav@outlook.com>
2023-06-12 16:18:15 -0400

    created view model

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -5,4 +5,27 @@
 //  Created by Dipti Yadav on 6/12/23.
 //
 
-import Foundation
+import SwiftUI
+
+
+class EmojiMemoryGame {
+    
+    // static make the variable global
+    // emojis is a global
+    // typed variable
+   static let emojis : Array<String> = ["🚞", "🚗",  "🛵", "✈️", "🚚", "🚑","🛺", "🚘","🏍️","🚊","🛴","🚁","🚔","🚎","🚓", "🚛","🚐","🚙","🚍","🛻","🏎️","⛵️","⛴️","🚁"]
+    
+    //typed function
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in emojis[pairIndex]}
+    }
+     
+    private var model: MemoryGame<String> = createMemoryGame()
+     
+    
+    // This is fresh copy because it is from struct
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+}
+
