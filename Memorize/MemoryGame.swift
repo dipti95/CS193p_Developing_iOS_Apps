83c781858e213ba5fd20e4d024187879c7e756b5
dipti <dipti.yadav@outlook.com>
2023-06-12 16:17:41 -0400

    created model

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+//CardContent type we don't care so we have to announce so that somebody uses this game then they can know what this "don't care" is.
+struct MemoryGame<CardContent> {
+    private(set) var cards: Array<Card>
+    
+    func choose(_ card: Card) {
+        
+    }
+    
+    init(numberOfPairsOfCards: Int, createCardContent:(Int) -> CardContent) {
+        cards = Array<Card>()
+        // add numberOfParirsOfCards * 2 cards to cards array
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = createCardContent(pairIndex)
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+        }
+        
+    }
+    
+    struct Card {
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
+        var content: CardContent
+    }
+}
