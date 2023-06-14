4c2adc00cda8a342744b93c4a7c5f3f0a472a617
dipti <dipti.yadav@outlook.com>
2023-06-13 22:29:36 -0400

    wrote all the logic

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -8,17 +8,30 @@
 import Foundation
 
 //CardContent type we don't care so we have to announce so that somebody uses this game then they can know what this "don't care" is.
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent> where CardContent: Equatable {
     private(set) var cards: Array<Card>
     
     
     // first of all the below variable value is none because we want for our game so that's why we put question mark which represent optional so that below variable will have value none or int
-    private var IndexOfTheOneAndOnlyFaceUpCard: Int?
+    private var indexOfTheOneAndOnlyFaceUpCard: Int?
      
    mutating func choose(_ card: Card) {
        //if let is the part of syntheic sugar which is provided by swift
-       if let chosenIndex = cards.firstIndex(where: {$0.id == card.id }){
-           
+     if let chosenIndex = cards.firstIndex(where: {$0.id == card.id }),    !cards[chosenIndex].isFaceUp,
+           !cards[chosenIndex].isMatched
+       {
+           if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
+               if cards[chosenIndex].content == cards[potentialMatchIndex].content {
+                   cards[chosenIndex].isMatched = true
+                   cards[potentialMatchIndex].isMatched = true
+               }
+               indexOfTheOneAndOnlyFaceUpCard = nil
+           } else {
+               for index in cards.indices {
+                   cards[index].isFaceUp = false
+               }
+               indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+           }
            
            cards[chosenIndex].isFaceUp.toggle()
        }
